a43137b7f78fd3107f90d233ddd61ba41c6ee171
Jordan <jordanians@yahoo.com>
2023-08-24 11:37:45 -0500

    Added 'sides' variable
    Added an input for the user to determine how many number of sides the dice will have.

--- dice_sim.py
+++ dice_sim.py
@@ -1,6 +1,7 @@
 import random
 
-def dice_choice()
-    
+# choose the number of sides your dice contains
+def dice_choice(sides)
+    sides = input('Enter number of sides: ')
 def roll_dice()
     #numebr = random.randint(1,6)
