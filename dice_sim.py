8757c227338a862402ec65d85d15d2fdbf5937e9
Jordan <jordanians@yahoo.com>
2023-08-27 17:46:21 -0500

    Update dice_sim.py

--- dice_sim.py
+++ dice_sim.py
@@ -1,12 +1,18 @@
 import random
+import re
+
 
 # choose the number of sides your dice contains
 def dice_choice():
     sides = input('Enter number of sides: ')
-    if map(sides.isalpha()):
+    if re.search('\d+',sides):
+        if int(sides) < 2:
+            print('You must chose a number greater than one')
+        elif int(sides) < 2:
+            print('Your choice in sides is:', sides)
+    elif re.search("\D+",sides):
         print('You must choose a number')
-    if sides.isnumeric():
-        print('Your choice in sides is:', sides)
+    
 def roll_dice():
 # numebr = random.randint(1,6)
     pass
