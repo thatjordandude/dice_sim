60001627bf304faa2500ffd98dc697649e0a7e9c
Jordan <jordanians@yahoo.com>
2023-08-29 10:16:52 -0500

    Initialized roll_dice function
    Create the roll dice function which rolls a random number based on the number of sides you chose.
    Also fixed the "your choice of sides is" message

--- dice_sim.py
+++ dice_sim.py
@@ -3,18 +3,21 @@ import re
 
 
 # choose the number of sides your dice contains
+sides = input('Enter number of sides: ')
 def dice_choice():
-    sides = input('Enter number of sides: ')
+    
     if re.search('\d+',sides):
         if int(sides) < 2:
             print('You must chose a number greater than one')
-        elif int(sides) < 2:
+        elif int(sides) > 2:
             print('Your choice in sides is:', sides)
     elif re.search("\D+",sides):
         print('You must choose a number')
-    
+    return sides
 def roll_dice():
 # numebr = random.randint(1,6)
-    pass
+    result = random.randint(1, int(sides))
+    print('You rolled:', result)
 
 dice_choice()
+roll_dice()
